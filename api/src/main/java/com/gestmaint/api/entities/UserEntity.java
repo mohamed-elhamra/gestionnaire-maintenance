173b24ae3515b5a58b132edb6523155c135e2554
melha <melhamra98@gmail.com>
2021-12-23 17:10:02 +0100

    first commit

---
+++ api/src/main/java/com/gestmaint/api/entities/UserEntity.java
@@ -0,0 +1,22 @@
+package com.gestmaint.api.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
