b4d44cc5a1c0a5f2978fafc4446400c8512ba6e3
melha <melhamra98@gmail.com>
2022-01-01 16:36:30 +0100

    Change config

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiURL: 'http://localhost:8081/api/v1.0',
-  //apiURL: 'http://192.168.76.76:8081/api/v1.0'
-  frontUrl: 'http://localhost:4200'
+  //apiURL: 'http://localhost:8081/api/v1.0',
+  apiURL: 'http://192.168.76.76:8081/api/v1.0',
+  //frontUrl: 'http://localhost:4200',
+  frontUrl: 'http://192.168.76.76:4200'
 };
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -4,9 +4,10 @@
 
 export const environment = {
   production: false,
-  apiURL: 'http://localhost:8081/api/v1.0',
-  //apiURL: 'http://192.168.76.76:8081/api/v1.0'
-  frontUrl: 'http://localhost:4200'
+  //apiURL: 'http://localhost:8081/api/v1.0',
+  apiURL: 'http://192.168.76.76:8081/api/v1.0',
+  //frontUrl: 'http://localhost:4200',
+  frontUrl: 'http://192.168.76.76:4200'
 };
 
 /*
